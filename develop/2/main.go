2e169b8bb26793eb89123d00203cd18caee79c2f
Egor <egor.@mail.ru>
2023-09-01 17:28:41 +0300

    2ex

--- develop/2/main.go
+++ develop/2/main.go
@@ -12,10 +12,15 @@ func main() {
 	mutex := sync.Mutex{}
 	wg.Add(len(array))
 	for _, num := range array {
+		// создаем анонимную функцию с аргументом num , которая будет выводить квадраты чисел
 		go func(num int) {
+			//Закрываем мьютекс
 			mutex.Lock()
+			//открываем мьютекс по выходу из функции
 			defer mutex.Unlock()
+			//выводим квадрат числа
 			fmt.Println(num * num)
+			//говорим что goroutine успешно завершилась
 			wg.Done()
 		}(num)
 	}
